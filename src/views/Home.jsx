f7f1f27925fb03af8aa92b1a183bed1a1421053b
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-09-18 13:20:34 +0500

    FIxed bug with manaher

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,10 +3,33 @@ import React, { useEffect, useState } from 'react'
 import { getDashboard } from 'services/product.service'
 import Table from "components/ui/Table"
 import { Link } from 'react-router-dom'
+import { getUsersDebtors } from 'services/founder-menejer.service'
 const { Tr, Th, Td, THead, TBody } = Table
 const Home = () => {
 	const [information, setInformation] = useState([])
+	const [managerInformation, setManagerInformation] = useState(null)
 	const role = localStorage.getItem("role")
+	// getClients(`?page=${page}&item=${selectValue}`)
+
+	const getManagerDashboard = async () => {
+		if (role === "manager") {
+			try {
+
+				let clientsResponse = await getUsersDebtors()
+				console.log(clientsResponse);
+				if (!clientsResponse.data?.data?.subscriptions.length) {
+					console.log(clientsResponse.data);
+
+					setManagerInformation("Ushbu Club bo'yicha qarzdorlar hozirda yo'q")
+				} else {
+					setManagerInformation(clientsResponse.data.subscriptions.length)
+				}
+			} catch (error) {
+				console.log(error);
+
+			}
+		}
+	}
 	const dashboard = async () => {
 		try {
 			let response = await getDashboard()
@@ -20,7 +43,7 @@ const Home = () => {
 
 	const dashboardManager = async () => {
 		try {
-			let response = await getDashboard()
+			let response = await getManagerDashboard()
 			console.log(response);
 
 			setInformation(response.data.clubs)
@@ -29,6 +52,8 @@ const Home = () => {
 		}
 	}
 	useEffect(() => {
+		console.log(role);
+
 		if (role === "superadmin") {
 
 			dashboard()
@@ -62,20 +87,7 @@ const Home = () => {
 
 			}
 			{role === "manager" ? <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-3">
-				{
-					information?.map((key) => {
-						return (
-							<Link to={`/club/${key._id}`}>
-								<Card className="w-full max-h-36">
-									<p className='text-2xl font-semibold font-mont '>{key?.name}</p>
-									<p className='text-xl dark:text-white'>Loyihalar soni: {key?.projects?.length}</p>
-									<p className='text-xl dark:text-white'>Url: {key?.url}</p>
-								</Card>
-							</Link>
-						)
-
-					})
-				}
+				<p>{managerInformation ? "Hozirda klub bo'yicha qarzdorlar yo'q" : managerInformation}</p>
 			</div> : null
 
 			}
